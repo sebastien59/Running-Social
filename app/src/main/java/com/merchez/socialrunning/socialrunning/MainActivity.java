28dbbbec77f9fbca7e20382772e31beb06e7e510
chahma <chahma94youssra@gmail.com>
2017-03-21 19:44:31 +0100

    okhttp

--- app/src/main/java/com/merchez/socialrunning/socialrunning/MainActivity.java
+++ app/src/main/java/com/merchez/socialrunning/socialrunning/MainActivity.java
@@ -1,13 +1,93 @@
 package com.merchez.socialrunning.socialrunning;
 
+import android.content.SharedPreferences;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
+import java.io.IOException;
+
+import okhttp3.*;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private Button btnConnect;
+    private EditText edtEmail;
+    private EditText edtPasse;
+    private CheckBox cbRemember;
+    private OkHttpClient client;
+    private String response;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        client = new OkHttpClient();
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnConnect = (Button) findViewById(R.id.button_connect);
+        edtEmail = (EditText) findViewById(R.id.email);
+        edtPasse = (EditText) findViewById(R.id.mdp);
+        cbRemember = (CheckBox) findViewById(R.id.mdp_checkbox);
+        btnConnect.setOnClickListener(this);
+    }
+
+
+    @Override
+    public void onClick(View view) {
+        String e = edtEmail.getText().toString();
+        String p = edtPasse.getText().toString();
+    if(btnConnect.getId() == view.getId()){
+        //action à faire quand on clique sur le bouton connexion
+        Log.i("debug","bouton de connexion");
+    }
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    private void loadContent() {
+        new AsyncTask<Void, Void, Void>() {
+            @Override
+            protected Void doInBackground(Void... params) {
+                try {
+                    response = APICall.GET(client, RequestBuilder.buildURL());
+                    //Parse the response string here
+                    Log.d("Response", response);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                return null;
+            }
+        }.execute();
+    }
+
+    private void attemptLogin(String url) {
+        final String email = edtEmail.getText().toString();
+        final String password = edtPasse.getText().toString();
+
+        new AsyncTask<String, Void, Void>() {
+            @Override
+            protected Void doInBackground(String... params) {
+
+                try {
+                    response = APICall.POST(
+                            client,
+                            HttpUrl.parse(params[0]),
+                            RequestBuilder.LoginBody(email, password,"token"));
+
+                    Log.d("Response", response);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                return null;
+            }
+        }.execute(url);
     }
 }
---
+++ app/src/main/java/com/merchez/socialrunning/socialrunning/RequestBuilder.java
@@ -0,0 +1,36 @@
+package com.merchez.socialrunning.socialrunning;
+
+import okhttp3.*;
+
+/**
+ * Created by Youssra on 21/03/2017.
+ */
+
+public class RequestBuilder {
+
+    //Login request body
+    public static RequestBody LoginBody(String username, String password, String token) {
+        return new FormBody.Builder()
+                .add("action", "login")
+                .add("format", "json")
+                .add("username", username)
+                .add("password", password)
+                .add("logintoken", token)
+                .build();
+    }
+
+    public static HttpUrl buildURL() {
+        return new HttpUrl.Builder()
+                .scheme("https") //http
+                .host("www.somehostname.com")
+                .addPathSegment("pathSegment")//adds "/pathSegment" at the end of hostname
+                .addQueryParameter("param1", "value1") //add query parameters to the URL
+                .addEncodedQueryParameter("encodedName", "encodedValue")//add encoded query parameters to the URL
+                .build();
+        /**
+         * The return URL:
+         *  https://www.somehostname.com/pathSegment?param1=value1&encodedName=encodedValue
+         */
+    }
+
+}
